14b3ef7d24d313640ba6212103349a5c4580b182
Benoit Queyron <queyronbenoit@gmail.com>
2024-05-14 16:56:32 +0200

    ex P1C5

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+nom = "Benoit"
+age = 30
+taille = 1.8
+est_etudiant = False
+
+# Afficher les valeurs des quatres variables
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+# Afficher les types des quatres variables
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
